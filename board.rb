6697d5bf1d1eb312666e0cf051b3341b0358e301
Andrew Kelly <kellyaj@gmail.com>
2012-12-23 15:36:19 -0600

    player cycling is primitive but functioning

--- board.rb
+++ board.rb
@@ -17,10 +17,11 @@ board.each do |z|
   column3 << z[2]
 end
 
+# need a way to express the columns and diagonals
 win_conditions = [ board[0], board[1], board[2], column1, column2, column3, [1,5,9], [7,5,3] ]
 player1_wins = ["X", "X", "X"]
 player2_wins = ["O", "O", "O"]
-
+current_player_wins = [@current_player, @current_player, @current_player]
 winner = false
 
 
@@ -53,27 +54,32 @@ def place_player_mark(board,pos,player)
   board[row][column] = player
 end
 
-initial_board
-# current_player = player1
-# puts "Player 1 please choose a numbered space to occupy"
-# choice = gets.chomp
-# place_player_mark(board,choice.to_i,current_player)
-# show_board(board)
-# current_player = player2
-# puts "Player 2 please choose a numbered space to occupy"
-# choice = gets.chomp
-# place_player_mark(board,choice.to_i,current_player)
-# show_board(board)
+def cycle_players(current_player)
+  player1 = "X"
+  player2 = "O"
+  if @current_player == player1
+    @current_player = player2
+  else
+    @current_player = player1
+  end
+  return @current_player
+end
 
+
+
+
+# the game loop, will continue until win condition is met
+initial_board
+@current_player = player2
 while winner == false
-  current_player = player1
+  cycle_players(@current_player)
   puts "Please choose a numbered space to occupy"
   choice = gets.chomp
-  place_player_mark(board,choice.to_i,current_player)
+  place_player_mark(board,choice.to_i,@current_player)
   show_board(board)
   if win_conditions.include? player1_wins || player2_wins
       winner = true
-      puts "The game is over"
+      puts "The game is over, #{@current_player} has won."
   end
 end
 
