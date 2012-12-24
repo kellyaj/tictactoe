5d5d3d9a9aa139a16666e2d4c27befbaa82832d1
Andrew Kelly <kellyaj@gmail.com>
2012-12-23 20:55:18 -0600

    slight cleanup

--- board.rb
+++ board.rb
@@ -7,28 +7,20 @@ board = [
 	["7", "8", "9"] 
 ]
 
-column1 = []
-column2 = []
-column3 = []
-
-board.each do |z|
-  column1 << z[0]
-  column2 << z[1]
-  column3 << z[2]
-end
 
 # need a way to express the columns and diagonals
-win_conditions = [ board[0], board[1], board[2], column1, column2, column3, [1,5,9], [7,5,3] ]
-player1_wins = ["X", "X", "X"]
-player2_wins = ["O", "O", "O"]
-current_player_wins = [@current_player, @current_player, @current_player]
-
 @winner_array = [true, true, true]
 
 def win_condition_row(board,current_player)
-  if board[0].collect { |w| w == current_player} == @winner_array
-    @winner = true
+  board.each do |board|
+    if board.collect { |w| w == current_player} == @winner_array
+      @winner = true
+    end
   end
+end
+
+def win_condition_column(board,current_player)
+
 end
 
 def initial_board
@@ -59,6 +51,7 @@ def place_player_mark(board,pos,player)
   board[row][column] = player
 end
 
+#changes the player. primitive and messy
 def cycle_players(current_player)
   player1 = "X"
   player2 = "O"
@@ -79,20 +72,16 @@ initial_board
 @current_player = player2
 while @winner == false
   cycle_players(@current_player)
-  puts "Please choose a numbered space to occupy"
+  puts " #{@current_player}, please choose a numbered space to occupy"
   choice = gets.chomp
   place_player_mark(board,choice.to_i,@current_player)
   show_board(board)
-  # if win_conditions.include? player1_wins || player2_wins
-  #     winner = true
-  #     puts "The game is over, #{@current_player} has won."
-  # end
   win_condition_row(board,@current_player)
+  win_condition_column(board,@current_player)
+  if @winner == true
+    puts "#{@current_player} has won the game."
+  end
 end
-
-
-# while loop until win condition is met
-# if space is occupied prevent marking
 
 
 
