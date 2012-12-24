f3714dba9b758d72cfd7b26ec3425d63a58b253a
Andrew Kelly <kellyaj@gmail.com>
2012-12-23 21:18:40 -0600

    occupied check primitively functioning

--- board.rb
+++ board.rb
@@ -45,11 +45,12 @@ end
 
 # converts user input into an actual index
 # ex. 6 --> board[1][2]
-def place_player_mark(board,pos,player) 
+def place_player_mark(board,pos,player)
   row = (pos - 1) / 3
   column = (pos - 1) % 3
   board[row][column] = player
 end
+
 
 #changes the player. primitive and messy
 def cycle_players(current_player)
@@ -63,19 +64,41 @@ def cycle_players(current_player)
   return @current_player
 end
 
+# validation that a space is not taken already
+def occupied_check(board,pos)
+  row = (pos - 1) / 3
+  column = (pos - 1) % 3
 
+  if board[row][column] == "X" || board[row][column] == "O"
+    @valid_move = false
+    puts "choose again, that place is occupied"
+  else
+    @valid_move = true
+  end
+end
 
-
-# the game loop, will continue until win condition is met
+# initialization stuff
 @winner = false
 initial_board
 @current_player = player2
+
+# the game loop, will continue until win condition is met
 while @winner == false
   cycle_players(@current_player)
   puts " #{@current_player}, please choose a numbered space to occupy"
-  choice = gets.chomp
+
+  # player input and move validation 
+  @valid_move = false
+  while @valid_move == false
+    choice = gets.chomp
+    occupied_check(board,choice.to_i)
+  end
+
+  # placing of player mark, assuming its valid
   place_player_mark(board,choice.to_i,@current_player)
   show_board(board)
+
+  # checking if game is over
   win_condition_row(board,@current_player)
   win_condition_column(board,@current_player)
   if @winner == true
